73e8ad2c2d119f8ec29f72ab3bf043679733fe6c
William O'Connor <bill@achieveinternet.com>
2016-06-07 14:31:01 -0700

    Reverting the path change.

--- hooks/common/post-code-update/slack.sh
+++ hooks/common/post-code-update/slack.sh
@@ -22,7 +22,7 @@ if [ -z "$7" ]; then username="Acquia Cloud"; else username="$7"; fi
 if [ -z "$8" ]; then channel="@csevb10"; else channel="$8"; fi
 
 # Load the Slack webhook URL (which is not stored in this repo).
-./slack_settings
+. $HOME/slack_settings
 
 echo $PWD
 
