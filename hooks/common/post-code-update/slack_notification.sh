b1db58dcd4bfa3da9786dd79485296ad34edd221
William O'Connor <bill@achieveinternet.com>
2016-06-14 09:41:42 -0700

    Remove outputs, update directory path one more time.

--- hooks/common/post-code-update/slack_notification.sh
+++ hooks/common/post-code-update/slack_notification.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env php
 <?php
 
-print getcwd() . "\n";
-print __DIR__ . "\n";
-require_once __DIR__ . "../../libraries/slack/slack_notification.php";
+require_once __DIR__ . "/../../libraries/slack/slack_notification.php";
