a149002450d5fbd787004d49777ec8aed0ab8ffe
William O'Connor <bill@achieveinternet.com>
2016-06-27 11:46:09 -0700

    More debug testing.

--- private/hooks/libraries/ci/acquia/pull-db.sh
+++ private/hooks/libraries/ci/acquia/pull-db.sh
@@ -3,6 +3,3 @@
 # Authenticate to Acquia and pull our DB from Acquia.
 drush acquia-update
 drush @$PAASSITE.$PAASENV sql-dump > db.sql
-
-# terminus auth login --machine-token=$MACHINETOKEN
-# terminus drush sql-dump --site=$PSITE --env=$PENV > db.sql
