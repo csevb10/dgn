b4ab673351c4c75a2a12b0ff96e6df652ba7d33a
William O'Connor <bill@achieveinternet.com>
2016-06-27 17:07:38 -0700

    Typos.

--- private/hooks/libraries/ci/acquia/pull-db.sh
+++ private/hooks/libraries/ci/acquia/pull-db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Authenticate to Acquia and pull our DB from Acquia.
-ssh $PAASSITE@$PAASENV@$PAASURL
+ssh $PAASSITE.$PAASENV@$PAASURL
 drush sql-dump $PAASITE@$PAASENV > db.sql
 exit
-scp $PAASSITE@$PAASENV@$PAASURL:db.sql db.sql
+scp $PAASSITE.$PAASENV@$PAASURL:db.sql db.sql
